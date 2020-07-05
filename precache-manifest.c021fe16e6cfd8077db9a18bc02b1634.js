03c64f772e78d6f548b51313ea12647402fb2d79
cvspinelli <cvspinelli09@gmail.com>
2020-07-05 12:15:21 +0100

    Updates

---
+++ precache-manifest.c021fe16e6cfd8077db9a18bc02b1634.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a736632a8cb724e81ed7376bcef051a2",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "61343143477225ba5d44",
+    "url": "/my-portfolio/static/css/main.23cb15e1.chunk.css"
+  },
+  {
+    "revision": "f5c8bb904e02a7dbf563",
+    "url": "/my-portfolio/static/js/2.21ae936c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-portfolio/static/js/2.21ae936c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61343143477225ba5d44",
+    "url": "/my-portfolio/static/js/main.6ed74b19.chunk.js"
+  },
+  {
+    "revision": "3c8ba0527ea07de4cb38",
+    "url": "/my-portfolio/static/js/runtime-main.8a084cdc.js"
+  },
+  {
+    "revision": "cff352ff6016266091b8c3ecb417c130",
+    "url": "/my-portfolio/static/media/30-Day-Guarantee-1.cff352ff.png"
+  },
+  {
+    "revision": "2121531819e7efeee3eeb47aaa86444f",
+    "url": "/my-portfolio/static/media/Logo-Spinelli-Web-Bold.21215318.png"
+  },
+  {
+    "revision": "055119c619ba6e0d7da9d1e37cdf24e7",
+    "url": "/my-portfolio/static/media/Logo-alone.055119c6.png"
+  },
+  {
+    "revision": "7ab44b36d7c3f2a9cd0328d4e2820936",
+    "url": "/my-portfolio/static/media/amazon-web-services-icon.7ab44b36.png"
+  },
+  {
+    "revision": "8d99ca2de8a004b3ad494cd901580c3e",
+    "url": "/my-portfolio/static/media/bootstrap.8d99ca2d.png"
+  },
+  {
+    "revision": "ce7864db9c8c05860ff8ebd5e509407a",
+    "url": "/my-portfolio/static/media/bundle-medium.ce7864db.png"
+  },
+  {
+    "revision": "b20cd3dc20d003177767211fb9521250",
+    "url": "/my-portfolio/static/media/buy-tickets.b20cd3dc.png"
+  },
+  {
+    "revision": "e326437586fdaaedf46a8367a0b4a5ea",
+    "url": "/my-portfolio/static/media/checkout.e3264375.png"
+  },
+  {
+    "revision": "c39f351dfc8b2f7fb4e92c5abda093e4",
+    "url": "/my-portfolio/static/media/classic-car.c39f351d.png"
+  },
+  {
+    "revision": "5c1d0be28faa6805e7a0795655381015",
+    "url": "/my-portfolio/static/media/cosmetics-set.5c1d0be2.png"
+  },
+  {
+    "revision": "340979ae987e866d4db9503bccd7f4c9",
+    "url": "/my-portfolio/static/media/cosmetics.340979ae.png"
+  },
+  {
+    "revision": "d37f43787753a7a5ce92f39b88613c13",
+    "url": "/my-portfolio/static/media/css3.d37f4378.png"
+  },
+  {
+    "revision": "99c81e2a9cafaf5e509929c678de8323",
+    "url": "/my-portfolio/static/media/demograpr.99c81e2a.png"
+  },
+  {
+    "revision": "ceb622c6581d059a4f10e0a413816a99",
+    "url": "/my-portfolio/static/media/docker-icon.ceb622c6.png"
+  },
+  {
+    "revision": "f16c16256df935fcff2d0bacc36edf8e",
+    "url": "/my-portfolio/static/media/dvd-large.f16c1625.png"
+  },
+  {
+    "revision": "15c650c5bdc00d1b313de02c85f0e96a",
+    "url": "/my-portfolio/static/media/ecom.15c650c5.png"
+  },
+  {
+    "revision": "e8252c9cdc1d3858d31abe451d3362bd",
+    "url": "/my-portfolio/static/media/github.e8252c9c.png"
+  },
+  {
+    "revision": "880c29095bf74cf4cec3b6e7bb73e56e",
+    "url": "/my-portfolio/static/media/green-background.880c2909.png"
+  },
+  {
+    "revision": "c9bddf573105b9b884dd6fb4cf89992f",
+    "url": "/my-portfolio/static/media/html5-icon.c9bddf57.png"
+  },
+  {
+    "revision": "9db14da644bf976f86b3718f7688fd75",
+    "url": "/my-portfolio/static/media/jest.9db14da6.png"
+  },
+  {
+    "revision": "ea3ce8802443621b6389c4b5ffb7def6",
+    "url": "/my-portfolio/static/media/mac.ea3ce880.jpg"
+  },
+  {
+    "revision": "2603b7c3eebd4b023f61d4c7d09b3cda",
+    "url": "/my-portfolio/static/media/nodejs-icon.2603b7c3.png"
+  },
+  {
+    "revision": "e0882eac5504a3c21165cf670aebf63f",
+    "url": "/my-portfolio/static/media/optin.e0882eac.png"
+  },
+  {
+    "revision": "67ca77369ee268426c721ea0cc8730c3",
+    "url": "/my-portfolio/static/media/postgresql.67ca7736.png"
+  },
+  {
+    "revision": "43444f415a923db5a681b560b3ccc436",
+    "url": "/my-portfolio/static/media/python-icon.43444f41.png"
+  },
+  {
+    "revision": "68b45d1588e13ec4992de83ed717fe23",
+    "url": "/my-portfolio/static/media/react-icon.68b45d15.png"
+  },
+  {
+    "revision": "98c45a4343e48639ce88ac8eebeb0fdd",
+    "url": "/my-portfolio/static/media/redis-icon.98c45a43.png"
+  },
+  {
+    "revision": "34a556df1168f1a683d0973151b1031b",
+    "url": "/my-portfolio/static/media/redux.34a556df.png"
+  },
+  {
+    "revision": "82c8ad6b82a654233a89285a314635ce",
+    "url": "/my-portfolio/static/media/sales.82c8ad6b.png"
+  },
+  {
+    "revision": "fa35d287fc04725c4621eb92e875043c",
+    "url": "/my-portfolio/static/media/sass.fa35d287.png"
+  },
+  {
+    "revision": "edc1b79149633b1bf810f8515538fbb9",
+    "url": "/my-portfolio/static/media/star-wars.edc1b791.png"
+  },
+  {
+    "revision": "dca6d853bb137fb2a5c02b60b21e44db",
+    "url": "/my-portfolio/static/media/teacher.dca6d853.png"
+  },
+  {
+    "revision": "f504eeecfcccf856639825adfc186980",
+    "url": "/my-portfolio/static/media/thankyoupage.f504eeec.png"
+  },
+  {
+    "revision": "b59acc99f9fbdcddc33eadfca9e97380",
+    "url": "/my-portfolio/static/media/upsell.b59acc99.png"
+  },
+  {
+    "revision": "e22ad37bcc5054113941788597301f1f",
+    "url": "/my-portfolio/static/media/weather.e22ad37b.png"
+  },
+  {
+    "revision": "2df548efb2589da7e8200389b56e3e86",
+    "url": "/my-portfolio/static/media/web-creation.2df548ef.jpg"
+  },
+  {
+    "revision": "80696e2731bc5e13b06895b3830cbfb5",
+    "url": "/my-portfolio/static/media/web-dev.80696e27.jpg"
+  },
+  {
+    "revision": "a822b31180694e1a071ac5e8d3b987bd",
+    "url": "/my-portfolio/static/media/wordpress-icon.a822b311.png"
+  }
+]);
